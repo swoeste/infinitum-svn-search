9dc3a060d4eb9d36660c18eec8cc3be91b67b828
Unknown <dev@swoeste.de>
2018-09-12 12:20:38 +0200

    improved error handling in case of a connection error to svn

--- core/svn.indexer/src/main/java/de/swoeste/infinitum/fw/core/bl/svn/indexer/SVNIndexWriter.java
+++ core/svn.indexer/src/main/java/de/swoeste/infinitum/fw/core/bl/svn/indexer/SVNIndexWriter.java
@@ -1,20 +1,16 @@
 /*
  * Copyright (C) 2017 Sebastian Woeste
  *
- * Licensed to Sebastian Woeste under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership. I license this file to You under
- * the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at
+ * Licensed to Sebastian Woeste under one or more contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership. I license this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package de.swoeste.infinitum.fw.core.bl.svn.indexer;
 
@@ -123,7 +119,7 @@ public class SVNIndexWriter implements IActionQueue {
 
     private final class SVNIndexWriterJob implements Runnable {
 
-        private final static long SIZE100MB = 104857600;
+        private static final long SIZE100MB = 104857600;
 
         /** {@inheritDoc} */
         @Override
@@ -158,9 +154,13 @@ public class SVNIndexWriter implements IActionQueue {
             } catch (IOException e) {
                 LOG.warn("TASK: An error occured while executing actions from queue", e);
 
+                // FIXME
+
             } catch (Exception e) {
                 LOG.error("TASK: An error occured while executing actions from queue", e);
+
                 // FIXME
+
             } finally {
                 close();
             }
--- core/svn.indexer/src/main/java/de/swoeste/infinitum/fw/core/bl/svn/indexer/SVNRepositoryReader.java
+++ core/svn.indexer/src/main/java/de/swoeste/infinitum/fw/core/bl/svn/indexer/SVNRepositoryReader.java
@@ -1,20 +1,16 @@
 /*
  * Copyright (C) 2017 Sebastian Woeste
  *
- * Licensed to Sebastian Woeste under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional
- * information regarding copyright ownership. I license this file to You under
- * the Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License
- * at
+ * Licensed to Sebastian Woeste under one or more contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership. I license this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package de.swoeste.infinitum.fw.core.bl.svn.indexer;
 
@@ -126,12 +122,10 @@ public class SVNRepositoryReader implements ISVNContentReader {
     }
 
     /**
-     * Sometimes the type of a node could not be get directly from the
-     * SVNLogEntryPath object. Then we need to connect with the svn repository
-     * and ask for the correct type of the node. This method check if the type
-     * could be get directly from the SVNLogEntryPath object and if not connect
-     * to the svn repository and ask for the type. If both went wrong UNKNOWN is
-     * returned as type.
+     * Sometimes the type of a node could not be get directly from the SVNLogEntryPath object. Then we need to connect
+     * with the svn repository and ask for the correct type of the node. This method check if the type could be get
+     * directly from the SVNLogEntryPath object and if not connect to the svn repository and ask for the type. If both
+     * went wrong UNKNOWN is returned as type.
      *
      * @param entry
      *            a SVNLogEntry
@@ -358,8 +352,7 @@ public class SVNRepositoryReader implements ISVNContentReader {
     }
 
     /**
-     * Fetch the size of the file specified by the given path in the given
-     * revision.
+     * Fetch the size of the file specified by the given path in the given revision.
      *
      * @param revision
      *            the revision of the file
